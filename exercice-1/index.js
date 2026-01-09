9a3b4bd61e2ea48e612b73476643cde8a91d64bc
smimoon <simonlaroche@orange.fr>
2026-01-09 13:12:09 +0000

    Save after correction

--- exercice-1/index.js
+++ exercice-1/index.js
@@ -1,15 +1,14 @@
-let table = [];
 function displayArray(array) {
-    for(let data of table) {
+    for(let data of array) {
         console.log(data);
     }
 }
 
-table = [1, 2, 3, 4, 5];
-displayArray();
+let table = [1, 2, 3, 4, 5];
+displayArray(table);
 table = [10, 20, 30, 40, 50];
-displayArray();
+displayArray(table);
 table = [5, 4, 3, 2, 1];
-displayArray();
+displayArray(table);
 table = [100, 200, 300, 400, 500];
-displayArray();
+displayArray(table);
