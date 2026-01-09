4ceec245a71ac9d1fd9c1c1182aa41c989a3679d
smimoon <simonlaroche@orange.fr>
2026-01-09 13:30:17 +0000

    Save end  exercice-2

---
+++ exercice-2/index.js
@@ -0,0 +1,16 @@
+function displayArrayReverse(array) {
+    console.log(array.reverse(array));
+
+}
+
+let table = [1, 2, 3, 4, 5];
+displayArrayReverse(table);
+
+table = [10, 20, 30, 40, 50];
+displayArrayReverse(table);
+
+table = [5, 4, 3, 2, 1];
+displayArrayReverse(table);
+
+table = [100, 200, 300, 400, 500];
+displayArrayReverse(table);
