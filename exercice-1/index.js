ba9d3109d6e59485d150c19fc50e9448147fb38d
smimoon <simonlaroche@orange.fr>
2026-01-09 12:52:40 +0000

    Save end exercice-1

---
+++ exercice-1/index.js
@@ -0,0 +1,15 @@
+let table = [];
+function displayArray(array) {
+    for(let data of table) {
+        console.log(data);
+    }
+}
+
+table = [1, 2, 3, 4, 5];
+displayArray();
+table = [10, 20, 30, 40, 50];
+displayArray();
+table = [5, 4, 3, 2, 1];
+displayArray();
+table = [100, 200, 300, 400, 500];
+displayArray();
