49c5398a0fe0521f443a9f51f03c94fd20a695ad
smimoon <simonlaroche@orange.fr>
2026-01-09 14:36:37 +0000

    Save end exercice-3

--- exercice-3/index.js
+++ exercice-3/index.js
@@ -1,9 +1,21 @@
 function findLongestWord(words) {
-    for(let word of words) {
-        console.log (word.length);
-
+    let x = 0;
+    let longest = "";
+    for(let i = 0; i<words.length; i++ ) {
+        if (words[i].length > x) {
+            x = words[i].length;
+            longest = words[i];
+        }
     }
+    return longest;
+
 }
 
 let test = ['hello', 'world', 'javascript', 'python', 'java'];
-findLongestWord(test);
+console.log(`The longest word is ${findLongestWord(test)}`);
+
+test = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+console.log(`The longest word is ${findLongestWord(test)}`);
+
+test = ['cat', 'dog', 'elephant', 'fox'];
+console.log(`The longest word is ${findLongestWord(test)}`);
