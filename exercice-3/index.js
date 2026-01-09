8a136e115522677115f518178803772625a88f47
smimoon <simonlaroche@orange.fr>
2026-01-09 13:59:30 +0000

    Save before break

---
+++ exercice-3/index.js
@@ -0,0 +1,9 @@
+function findLongestWord(words) {
+    for(let word of words) {
+        console.log (word.length);
+
+    }
+}
+
+let test = ['hello', 'world', 'javascript', 'python', 'java'];
+findLongestWord(test);
